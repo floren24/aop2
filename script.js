b4a4d501966308bfd2e374ba6991c8323859c7ed
floren24 <florencia.gabrielle@santa-laurensia.sch.id>
2025-04-25 19:28:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function tampil() {
+	document.getElementById('alert').style.display = "block";
+	document.getElementById('overlay').style.display = "block";
+}
+
+function tutup() {
+	document.getElementById('alert').style.display = "none";
+	document.getElementById('overlay').style.display = "none";
+}
